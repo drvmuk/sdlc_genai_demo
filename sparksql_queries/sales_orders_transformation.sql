3da1b1cbb224f65bb5f7ef0e31089e37b2ce3cc9
Dhrubojyoti Mukherjee <80147836+drvmuk@users.noreply.github.com>
2025-08-18 21:39:17 +0530

    Initial commit by library

--- sparksql_queries/sales_orders_transformation.sql
+++ sparksql_queries/sales_orders_transformation.sql
@@ -1,109 +1,285 @@
 %sql
--- Stage 1: Retrieve and join data from b_lots.plooh and b_lots.plool
--- Create intermediate table for sales orders
-CREATE OR REPLACE TABLE s_shared.sales_orders_hdr_itm_lots
-USING DELTA
-LOCATION '/mnt/data/s_shared/sales_orders_hdr_itm_lots'
-AS
-SELECT 
-  h.refto AS OrderNumber,
-  h.refty AS OrderType,
-  h.bukrs AS SalesOrgCompanyCode,
-  h.vkorg AS SalesOrganization,
-  h.vtweg AS DistributionChannel,
-  h.spart AS Division,
-  h.auart AS SalesDocumentType,
-  h.kunnr AS SoldToParty,
-  h.kunag AS ShipToParty,
-  h.bstkd AS CustomerPurchaseOrderNumber,
-  h.bstdk AS CustomerPurchaseOrderDate,
-  h.datoxc AS OrderCreationDate,
-  h.audat AS DocumentDate,
-  h.vdatu AS RequestedDeliveryDate,
-  h.erdat AS CreatedOn,
-  h.erzet AS EntryTime,
-  h.ernam AS CreatedBy,
-  i.posnr AS ItemNumber,
-  i.matnr AS MaterialNumber,
-  i.arktx AS ItemDescription,
-  i.kwmeng AS OrderQuantity,
-  i.vrkme AS SalesUnit,
-  i.werks AS Plant,
-  i.lgort AS StorageLocation,
-  i.vstel AS ShippingPoint,
-  i.route AS Route,
-  i.lprio AS DeliveryPriority,
-  i.sdabw AS SpecialProcessingIndicator,
-  i.kzazu AS OrderCombinationIndicator,
-  i.netwr AS NetValue,
-  i.waerk AS Currency,
-  h.DATA_OPERATION,
-  i.DATA_OPERATION AS ITEM_DATA_OPERATION
-FROM b_lots.plooh h
-JOIN b_lots.plool i 
-  ON h.refto = i.refto 
-  AND h.refno = i.refno
-WHERE h.DATA_OPERATION <> 'D';
+-- Step 1: Create intermediate table s_shared.sales_orders_hdr_itm_lots
+-- First, drop the table if it exists to avoid conflicts
+DROP TABLE IF EXISTS s_shared.sales_orders_hdr_itm_lots;
 
--- Stage 4-6: Create main sales_orders table with additional joins and transformations
-%sql
-CREATE OR REPLACE TABLE sales_orders
-USING DELTA
-LOCATION '/mnt/data/sales_orders'
-AS
+-- Create the intermediate table by joining header and item data
+CREATE TABLE s_shared.sales_orders_hdr_itm_lots AS
 SELECT 
-  so.OrderNumber,
-  so.OrderType,
-  so.SalesOrgCompanyCode,
-  so.SalesOrganization,
-  so.DistributionChannel,
-  dc.vtext AS SalesDistributionChannel,
-  so.Division,
-  so.SalesDocumentType,
-  so.SoldToParty,
-  stc.name1 AS SoldToName,
-  so.ShipToParty,
-  shc.name1 AS ShipToName,
-  so.CustomerPurchaseOrderNumber,
-  to_date(from_unixtime(unix_timestamp(cast(so.CustomerPurchaseOrderDate as string), 'yyyyMMdd'))) AS CustomerPurchaseOrderDate,
-  to_date(from_unixtime(unix_timestamp(cast(so.OrderCreationDate as string), 'yyyyMMdd'))) AS OrderCreationDate,
-  to_date(from_unixtime(unix_timestamp(cast(so.DocumentDate as string), 'yyyyMMdd'))) AS DocumentDate,
-  to_date(from_unixtime(unix_timestamp(cast(so.RequestedDeliveryDate as string), 'yyyyMMdd'))) AS RequestedDeliveryDate,
-  to_date(from_unixtime(unix_timestamp(cast(so.CreatedOn as string), 'yyyyMMdd'))) AS CreatedOn,
-  so.EntryTime,
-  so.CreatedBy,
-  so.ItemNumber,
-  so.MaterialNumber,
-  so.ItemDescription,
-  so.OrderQuantity,
-  so.SalesUnit,
-  so.Plant,
-  so.StorageLocation,
-  so.ShippingPoint,
-  so.Route,
-  so.DeliveryPriority,
-  so.SpecialProcessingIndicator,
-  so.OrderCombinationIndicator,
-  so.NetValue,
-  so.Currency,
-  CASE 
-    WHEN pal.werks IS NOT NULL THEN pal.werks
-    WHEN psl.werks IS NOT NULL THEN psl.werks
-    ELSE so.Plant
-  END AS SiteId,
-  current_timestamp() AS ETL_TIMESTAMP
-FROM s_shared.sales_orders_hdr_itm_lots so
-LEFT JOIN b_lots.plcam dc 
-  ON so.DistributionChannel = dc.vtweg
-  AND so.SalesOrganization = dc.vkorg
-LEFT JOIN b_lots.plcbe stc 
-  ON so.SoldToParty = stc.kunnr
-LEFT JOIN b_lots.plcbe shc 
-  ON so.ShipToParty = shc.kunnr
-LEFT JOIN b_lots.ploal pal 
-  ON so.Plant = pal.werks
-LEFT JOIN b_lots.plosl psl 
-  ON so.StorageLocation = psl.lgort
-  AND so.Plant = psl.werks
-WHERE so.DATA_OPERATION <> 'D'
-  AND year(cast(to_date(from_unixtime(unix_timestamp(cast(so.OrderCreationDate as string), 'yyyyMMdd'))) as date)) >= year(current_date()) - 3;
+    h.SALES_ORDER_NUMBER,
+    h.SALES_ORDER_TYPE,
+    h.SALES_ORDER_DATE,
+    h.CUSTOMER_NUMBER,
+    h.CUSTOMER_NAME,
+    h.CUSTOMER_PO_NUMBER,
+    h.REQUESTED_DELIVERY_DATE,
+    h.PROMISED_DELIVERY_DATE,
+    h.SALES_PERSON,
+    h.SALES_OFFICE,
+    h.SALES_DIVISION,
+    h.SALES_GROUP,
+    h.SALES_DISTRICT,
+    h.DISTRIBUTION_CHANNEL,
+    h.DIVISION,
+    h.BILLING_BLOCK,
+    h.DELIVERY_BLOCK,
+    h.CREDIT_STATUS,
+    h.OVERALL_STATUS,
+    h.REJECTION_STATUS,
+    h.CREATED_BY,
+    h.CREATED_DATE,
+    h.LAST_CHANGED_BY,
+    h.LAST_CHANGED_DATE,
+    h.CURRENCY_CODE,
+    h.EXCHANGE_RATE,
+    h.NET_VALUE,
+    h.TAX_AMOUNT,
+    h.GROSS_VALUE,
+    i.SALES_ORDER_ITEM,
+    i.MATERIAL_NUMBER,
+    i.MATERIAL_DESCRIPTION,
+    i.ORDER_QUANTITY,
+    i.SALES_UNIT,
+    i.NET_PRICE,
+    i.NET_VALUE AS ITEM_NET_VALUE,
+    i.PLANT,
+    i.STORAGE_LOCATION,
+    i.SHIPPING_POINT,
+    i.ROUTE,
+    i.CUSTOMER_MATERIAL_NUMBER,
+    i.ITEM_CATEGORY,
+    i.ITEM_TYPE,
+    i.PRICING_GROUP,
+    i.PRODUCT_HIERARCHY,
+    i.MATERIAL_GROUP,
+    i.MATERIAL_GROUP_1,
+    i.MATERIAL_GROUP_2,
+    i.MATERIAL_GROUP_3,
+    i.MATERIAL_GROUP_4,
+    i.MATERIAL_GROUP_5,
+    i.BATCH_NUMBER,
+    i.CONFIRMED_QUANTITY,
+    i.DELIVERED_QUANTITY,
+    i.OPEN_QUANTITY,
+    i.REJECTED_QUANTITY,
+    i.REASON_FOR_REJECTION,
+    i.ITEM_STATUS,
+    i.ITEM_BILLING_BLOCK,
+    i.ITEM_DELIVERY_BLOCK,
+    i.REQUESTED_DELIVERY_DATE AS ITEM_REQUESTED_DELIVERY_DATE,
+    i.CONFIRMED_DELIVERY_DATE,
+    i.ACTUAL_DELIVERY_DATE,
+    i.GOODS_ISSUE_DATE,
+    i.CREATED_BY AS ITEM_CREATED_BY,
+    i.CREATED_DATE AS ITEM_CREATED_DATE,
+    i.LAST_CHANGED_BY AS ITEM_LAST_CHANGED_BY,
+    i.LAST_CHANGED_DATE AS ITEM_LAST_CHANGED_DATE,
+    current_timestamp() AS ETL_CREATED_DATE,
+    'INITIAL_LOAD' AS ETL_CREATED_BY,
+    current_timestamp() AS ETL_UPDATED_DATE,
+    'INITIAL_LOAD' AS ETL_UPDATED_BY
+FROM 
+    b_lots.plooh h
+JOIN 
+    b_lots.plool i ON h.SALES_ORDER_NUMBER = i.SALES_ORDER_NUMBER
+WHERE 
+    h.DATA_OPERATION <> 'D'
+    AND i.DATA_OPERATION <> 'D';
+
+-- Step 2: Create main sales_orders table
+DROP TABLE IF EXISTS sales_orders;
+
+CREATE TABLE sales_orders AS
+WITH site_info AS (
+    SELECT 
+        s.SALES_ORDER_NUMBER,
+        s.SALES_ORDER_ITEM,
+        a.SITE_ID,
+        l.LOCATION_ID
+    FROM 
+        s_shared.sales_orders_hdr_itm_lots s
+    LEFT JOIN 
+        b_lots.ploal a ON s.PLANT = a.PLANT_CODE
+    LEFT JOIN 
+        b_lots.plosl l ON s.STORAGE_LOCATION = l.STORAGE_LOCATION_CODE AND s.PLANT = l.PLANT_CODE
+),
+campaign_info AS (
+    SELECT 
+        c.SALES_ORDER_NUMBER,
+        c.SALES_ORDER_ITEM,
+        c.CAMPAIGN_ID,
+        c.CAMPAIGN_NAME,
+        c.PROMOTION_ID,
+        c.PROMOTION_NAME
+    FROM 
+        b_lots.plcam c
+),
+payment_info AS (
+    SELECT 
+        p.CUSTOMER_NUMBER,
+        p.PAYMENT_METHOD,
+        p.PAYMENT_TERMS,
+        p.PAYMENT_DAYS
+    FROM 
+        b_lots.plpms p
+),
+hierarchy_info AS (
+    SELECT 
+        h.MATERIAL_NUMBER,
+        h.PRODUCT_HIERARCHY_LEVEL_1,
+        h.PRODUCT_HIERARCHY_LEVEL_2,
+        h.PRODUCT_HIERARCHY_LEVEL_3,
+        h.PRODUCT_HIERARCHY_LEVEL_4,
+        h.PRODUCT_HIERARCHY_LEVEL_5
+    FROM 
+        b_lots.phihm h
+),
+eco_info AS (
+    SELECT 
+        e.MATERIAL_NUMBER,
+        e.ECO_TIER,
+        e.SUSTAINABILITY_SCORE,
+        e.CARBON_FOOTPRINT
+    FROM 
+        b_lots.lecotw1 e
+),
+currency_conversion AS (
+    SELECT 
+        cc.CURRENCY_CODE,
+        cc.CONVERSION_DATE,
+        cc.CONVERSION_RATE,
+        cc.TARGET_CURRENCY
+    FROM 
+        s_shared.currency_conversion cc
+    WHERE 
+        cc.TARGET_CURRENCY = 'USD'
+)
+SELECT 
+    s.SALES_ORDER_NUMBER,
+    s.SALES_ORDER_ITEM,
+    s.SALES_ORDER_TYPE,
+    s.SALES_ORDER_DATE,
+    s.CUSTOMER_NUMBER,
+    s.CUSTOMER_NAME,
+    s.CUSTOMER_PO_NUMBER,
+    s.REQUESTED_DELIVERY_DATE,
+    s.PROMISED_DELIVERY_DATE,
+    s.SALES_PERSON,
+    s.SALES_OFFICE,
+    s.SALES_DIVISION,
+    s.SALES_GROUP,
+    s.SALES_DISTRICT,
+    s.DISTRIBUTION_CHANNEL,
+    s.DIVISION,
+    s.BILLING_BLOCK,
+    s.DELIVERY_BLOCK,
+    s.CREDIT_STATUS,
+    s.OVERALL_STATUS,
+    s.REJECTION_STATUS,
+    s.MATERIAL_NUMBER,
+    s.MATERIAL_DESCRIPTION,
+    s.ORDER_QUANTITY,
+    s.SALES_UNIT,
+    s.NET_PRICE,
+    s.ITEM_NET_VALUE,
+    s.PLANT,
+    s.STORAGE_LOCATION,
+    s.SHIPPING_POINT,
+    s.ROUTE,
+    s.CUSTOMER_MATERIAL_NUMBER,
+    s.ITEM_CATEGORY,
+    s.ITEM_TYPE,
+    s.PRICING_GROUP,
+    s.PRODUCT_HIERARCHY,
+    s.MATERIAL_GROUP,
+    s.MATERIAL_GROUP_1,
+    s.MATERIAL_GROUP_2,
+    s.MATERIAL_GROUP_3,
+    s.MATERIAL_GROUP_4,
+    s.MATERIAL_GROUP_5,
+    s.BATCH_NUMBER,
+    s.CONFIRMED_QUANTITY,
+    s.DELIVERED_QUANTITY,
+    s.OPEN_QUANTITY,
+    s.REJECTED_QUANTITY,
+    s.REASON_FOR_REJECTION,
+    s.ITEM_STATUS,
+    s.ITEM_BILLING_BLOCK,
+    s.ITEM_DELIVERY_BLOCK,
+    s.ITEM_REQUESTED_DELIVERY_DATE,
+    s.CONFIRMED_DELIVERY_DATE,
+    s.ACTUAL_DELIVERY_DATE,
+    s.GOODS_ISSUE_DATE,
+    s.CURRENCY_CODE,
+    s.EXCHANGE_RATE,
+    s.NET_VALUE,
+    s.TAX_AMOUNT,
+    s.GROSS_VALUE,
+    -- Site information derived from joins
+    si.SITE_ID,
+    si.LOCATION_ID,
+    -- Campaign information
+    ci.CAMPAIGN_ID,
+    ci.CAMPAIGN_NAME,
+    ci.PROMOTION_ID,
+    ci.PROMOTION_NAME,
+    -- Payment information
+    pi.PAYMENT_METHOD,
+    pi.PAYMENT_TERMS,
+    pi.PAYMENT_DAYS,
+    -- Product hierarchy details
+    hi.PRODUCT_HIERARCHY_LEVEL_1,
+    hi.PRODUCT_HIERARCHY_LEVEL_2,
+    hi.PRODUCT_HIERARCHY_LEVEL_3,
+    hi.PRODUCT_HIERARCHY_LEVEL_4,
+    hi.PRODUCT_HIERARCHY_LEVEL_5,
+    -- Eco information
+    ei.ECO_TIER,
+    ei.SUSTAINABILITY_SCORE,
+    ei.CARBON_FOOTPRINT,
+    -- Currency conversion
+    CASE 
+        WHEN cc.CONVERSION_RATE IS NOT NULL THEN s.NET_VALUE * cc.CONVERSION_RATE
+        ELSE s.NET_VALUE
+    END AS NET_VALUE_USD,
+    CASE 
+        WHEN cc.CONVERSION_RATE IS NOT NULL THEN s.GROSS_VALUE * cc.CONVERSION_RATE
+        ELSE s.GROSS_VALUE
+    END AS GROSS_VALUE_USD,
+    -- Calculated fields
+    DATEDIFF(s.ACTUAL_DELIVERY_DATE, s.REQUESTED_DELIVERY_DATE) AS DELIVERY_LEAD_TIME,
+    CASE 
+        WHEN s.ACTUAL_DELIVERY_DATE <= s.PROMISED_DELIVERY_DATE THEN 1
+        ELSE 0
+    END AS ON_TIME_DELIVERY_FLAG,
+    CASE 
+        WHEN s.REJECTED_QUANTITY > 0 THEN s.REJECTED_QUANTITY / NULLIF(s.ORDER_QUANTITY, 0)
+        ELSE 0
+    END AS REJECTION_RATE,
+    -- Audit columns
+    s.CREATED_BY,
+    s.CREATED_DATE,
+    s.LAST_CHANGED_BY,
+    s.LAST_CHANGED_DATE,
+    current_timestamp() AS ETL_CREATED_DATE,
+    'INITIAL_LOAD' AS ETL_CREATED_BY,
+    current_timestamp() AS ETL_UPDATED_DATE,
+    'INITIAL_LOAD' AS ETL_UPDATED_BY
+FROM 
+    s_shared.sales_orders_hdr_itm_lots s
+LEFT JOIN 
+    site_info si ON s.SALES_ORDER_NUMBER = si.SALES_ORDER_NUMBER AND s.SALES_ORDER_ITEM = si.SALES_ORDER_ITEM
+LEFT JOIN 
+    campaign_info ci ON s.SALES_ORDER_NUMBER = ci.SALES_ORDER_NUMBER AND s.SALES_ORDER_ITEM = ci.SALES_ORDER_ITEM
+LEFT JOIN 
+    payment_info pi ON s.CUSTOMER_NUMBER = pi.CUSTOMER_NUMBER
+LEFT JOIN 
+    hierarchy_info hi ON s.MATERIAL_NUMBER = hi.MATERIAL_NUMBER
+LEFT JOIN 
+    eco_info ei ON s.MATERIAL_NUMBER = ei.MATERIAL_NUMBER
+LEFT JOIN 
+    currency_conversion cc ON s.CURRENCY_CODE = cc.CURRENCY_CODE AND DATE(s.SALES_ORDER_DATE) = DATE(cc.CONVERSION_DATE);
+
+-- Optimize the tables for better query performance
+OPTIMIZE s_shared.sales_orders_hdr_itm_lots ZORDER BY (SALES_ORDER_NUMBER, SALES_ORDER_DATE);
+OPTIMIZE sales_orders ZORDER BY (SALES_ORDER_NUMBER, SALES_ORDER_DATE, CUSTOMER_NUMBER);
--- sqlvalidation_queries/sales_orders_validation.sql
+++ sqlvalidation_queries/sales_orders_validation.sql
@@ -1,59 +1,170 @@
 %sql
--- Validate source data availability
-SELECT 'plooh' as table_name, count(*) as record_count FROM b_lots.plooh
-UNION ALL
-SELECT 'plool' as table_name, count(*) as record_count FROM b_lots.plool
-UNION ALL
-SELECT 'plcam' as table_name, count(*) as record_count FROM b_lots.plcam
-UNION ALL
-SELECT 'plcbe' as table_name, count(*) as record_count FROM b_lots.plcbe
-UNION ALL
-SELECT 'ploal' as table_name, count(*) as record_count FROM b_lots.ploal
-UNION ALL
-SELECT 'plosl' as table_name, count(*) as record_count FROM b_lots.plosl;
-
--- Validate intermediate table creation
-%sql
-SELECT count(*) as record_count FROM s_shared.sales_orders_hdr_itm_lots;
-
--- Validate data quality in intermediate table
-%sql
+-- Validation Query 1: Check record counts in source and target tables
 SELECT 
-  COUNT(*) as total_records,
-  SUM(CASE WHEN OrderNumber IS NULL THEN 1 ELSE 0 END) as null_order_numbers,
-  SUM(CASE WHEN OrderType IS NULL THEN 1 ELSE 0 END) as null_order_types,
-  SUM(CASE WHEN DATA_OPERATION = 'D' THEN 1 ELSE 0 END) as deleted_records
-FROM s_shared.sales_orders_hdr_itm_lots;
-
--- Validate final table creation
-%sql
-SELECT count(*) as record_count FROM sales_orders;
-
--- Validate data quality in final table
-%sql
+    'b_lots.plooh' AS table_name,
+    COUNT(*) AS total_records,
+    COUNT(CASE WHEN DATA_OPERATION <> 'D' THEN 1 END) AS valid_records
+FROM 
+    b_lots.plooh
+UNION ALL
 SELECT 
-  COUNT(*) as total_records,
-  SUM(CASE WHEN OrderNumber IS NULL THEN 1 ELSE 0 END) as null_order_numbers,
-  SUM(CASE WHEN OrderCreationDate IS NULL THEN 1 ELSE 0 END) as null_creation_dates,
-  MIN(OrderCreationDate) as min_creation_date,
-  MAX(OrderCreationDate) as max_creation_date
-FROM sales_orders;
-
--- Validate join quality
-%sql
+    'b_lots.plool' AS table_name,
+    COUNT(*) AS total_records,
+    COUNT(CASE WHEN DATA_OPERATION <> 'D' THEN 1 END) AS valid_records
+FROM 
+    b_lots.plool
+UNION ALL
 SELECT 
-  COUNT(*) as total_records,
-  SUM(CASE WHEN SalesDistributionChannel IS NULL THEN 1 ELSE 0 END) as missing_distribution_channel,
-  SUM(CASE WHEN SoldToName IS NULL THEN 1 ELSE 0 END) as missing_sold_to_name,
-  SUM(CASE WHEN ShipToName IS NULL THEN 1 ELSE 0 END) as missing_ship_to_name,
-  SUM(CASE WHEN SiteId IS NULL THEN 1 ELSE 0 END) as missing_site_id
-FROM sales_orders;
-
--- Validate date filter is working correctly (last 3 years of data)
-%sql
+    's_shared.sales_orders_hdr_itm_lots' AS table_name,
+    COUNT(*) AS total_records,
+    COUNT(*) AS valid_records
+FROM 
+    s_shared.sales_orders_hdr_itm_lots
+UNION ALL
 SELECT 
-  YEAR(OrderCreationDate) as order_year,
-  COUNT(*) as record_count
-FROM sales_orders
-GROUP BY YEAR(OrderCreationDate)
-ORDER BY order_year DESC;
+    'sales_orders' AS table_name,
+    COUNT(*) AS total_records,
+    COUNT(*) AS valid_records
+FROM 
+    sales_orders;
+
+%sql
+-- Validation Query 2: Check for null values in key columns of intermediate table
+SELECT 
+    COUNT(*) AS total_records,
+    COUNT(CASE WHEN SALES_ORDER_NUMBER IS NULL THEN 1 END) AS null_sales_order_number,
+    COUNT(CASE WHEN SALES_ORDER_ITEM IS NULL THEN 1 END) AS null_sales_order_item,
+    COUNT(CASE WHEN MATERIAL_NUMBER IS NULL THEN 1 END) AS null_material_number,
+    COUNT(CASE WHEN CUSTOMER_NUMBER IS NULL THEN 1 END) AS null_customer_number
+FROM 
+    s_shared.sales_orders_hdr_itm_lots;
+
+%sql
+-- Validation Query 3: Check for null values in key columns of final table
+SELECT 
+    COUNT(*) AS total_records,
+    COUNT(CASE WHEN SALES_ORDER_NUMBER IS NULL THEN 1 END) AS null_sales_order_number,
+    COUNT(CASE WHEN SALES_ORDER_ITEM IS NULL THEN 1 END) AS null_sales_order_item,
+    COUNT(CASE WHEN MATERIAL_NUMBER IS NULL THEN 1 END) AS null_material_number,
+    COUNT(CASE WHEN CUSTOMER_NUMBER IS NULL THEN 1 END) AS null_customer_number,
+    COUNT(CASE WHEN SITE_ID IS NULL THEN 1 END) AS null_site_id
+FROM 
+    sales_orders;
+
+%sql
+-- Validation Query 4: Check for data consistency between intermediate and final tables
+SELECT 
+    'Record count match' AS validation_check,
+    CASE 
+        WHEN (SELECT COUNT(*) FROM s_shared.sales_orders_hdr_itm_lots) = 
+             (SELECT COUNT(*) FROM sales_orders)
+        THEN 'PASSED'
+        ELSE 'FAILED'
+    END AS validation_result
+UNION ALL
+SELECT 
+    'Sales order totals match' AS validation_check,
+    CASE 
+        WHEN (SELECT COUNT(DISTINCT SALES_ORDER_NUMBER) FROM s_shared.sales_orders_hdr_itm_lots) = 
+             (SELECT COUNT(DISTINCT SALES_ORDER_NUMBER) FROM sales_orders)
+        THEN 'PASSED'
+        ELSE 'FAILED'
+    END AS validation_result;
+
+%sql
+-- Validation Query 5: Check for data quality in calculated fields
+SELECT 
+    COUNT(*) AS total_records,
+    COUNT(CASE WHEN DELIVERY_LEAD_TIME < 0 THEN 1 END) AS negative_lead_time,
+    COUNT(CASE WHEN REJECTION_RATE < 0 OR REJECTION_RATE > 1 THEN 1 END) AS invalid_rejection_rate,
+    COUNT(CASE WHEN ON_TIME_DELIVERY_FLAG NOT IN (0, 1) THEN 1 END) AS invalid_on_time_flag
+FROM 
+    sales_orders;
+
+%sql
+-- Validation Query 6: Check for currency conversion accuracy
+SELECT 
+    CURRENCY_CODE,
+    COUNT(*) AS record_count,
+    AVG(NET_VALUE) AS avg_net_value,
+    AVG(NET_VALUE_USD) AS avg_net_value_usd,
+    AVG(CASE WHEN CURRENCY_CODE = 'USD' THEN NET_VALUE_USD / NULLIF(NET_VALUE, 0) ELSE NULL END) AS avg_conversion_rate_usd
+FROM 
+    sales_orders
+GROUP BY 
+    CURRENCY_CODE
+ORDER BY 
+    CURRENCY_CODE;
+
+%sql
+-- Validation Query 7: Check for data distribution by key dimensions
+SELECT 
+    SALES_ORDER_TYPE,
+    COUNT(*) AS record_count,
+    COUNT(DISTINCT SALES_ORDER_NUMBER) AS order_count,
+    SUM(NET_VALUE_USD) AS total_net_value_usd
+FROM 
+    sales_orders
+GROUP BY 
+    SALES_ORDER_TYPE
+ORDER BY 
+    record_count DESC;
+
+%sql
+-- Validation Query 8: Check for data distribution by time
+SELECT 
+    DATE_TRUNC('month', SALES_ORDER_DATE) AS order_month,
+    COUNT(*) AS record_count,
+    COUNT(DISTINCT SALES_ORDER_NUMBER) AS order_count,
+    SUM(NET_VALUE_USD) AS total_net_value_usd
+FROM 
+    sales_orders
+GROUP BY 
+    DATE_TRUNC('month', SALES_ORDER_DATE)
+ORDER BY 
+    order_month;
+
+%sql
+-- Validation Query 9: Check for orphaned records in the final table
+SELECT 
+    'Orders without site info' AS check_type,
+    COUNT(*) AS record_count
+FROM 
+    sales_orders
+WHERE 
+    SITE_ID IS NULL
+UNION ALL
+SELECT 
+    'Orders without campaign info' AS check_type,
+    COUNT(*) AS record_count
+FROM 
+    sales_orders
+WHERE 
+    CAMPAIGN_ID IS NULL
+UNION ALL
+SELECT 
+    'Orders without payment info' AS check_type,
+    COUNT(*) AS record_count
+FROM 
+    sales_orders
+WHERE 
+    PAYMENT_METHOD IS NULL
+UNION ALL
+SELECT 
+    'Orders without hierarchy info' AS check_type,
+    COUNT(*) AS record_count
+FROM 
+    sales_orders
+WHERE 
+    PRODUCT_HIERARCHY_LEVEL_1 IS NULL;
+
+%sql
+-- Validation Query 10: Check for data freshness
+SELECT 
+    MIN(SALES_ORDER_DATE) AS oldest_order_date,
+    MAX(SALES_ORDER_DATE) AS newest_order_date,
+    DATEDIFF(CURRENT_DATE(), MAX(SALES_ORDER_DATE)) AS days_since_latest_order,
+    MIN(ETL_CREATED_DATE) AS oldest_etl_date,
+    MAX(ETL_CREATED_DATE) AS newest_etl_date
+FROM 
+    sales_orders;
